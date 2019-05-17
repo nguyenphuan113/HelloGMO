029d1dec120c938b042adb4e4f523f62c9700191
unknown <amogan113@gmail.com>
2019-05-17 17:59:55 +0700

    commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,9 @@ public static void main(String[] args){
 	for(int i=1;i<=10;i++){
 		System.out.println("Hello GMO"+i);
 	}
+	
+	for(int i=1;i<=10;i++){
+		System.out.println("Hello GMO"+i);
+	}
 }
 }
